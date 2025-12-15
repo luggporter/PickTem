f96bc0d282d221d62ec3eb61902f0408f64799f3
joseph-artificial <joseph@artificial.sc>
2025-12-15 16:21:54 +0900

    매거진 추가

--- src/components/ads/AdRotator.tsx
+++ src/components/ads/AdRotator.tsx
@@ -1,7 +1,6 @@
 import { Box } from '@chakra-ui/react'
 import AdSense from '../AdSense'
 import KakaoAdFit from './KakaoAdFit'
-import MediaNet from './MediaNet'
 
 interface AdRotatorProps {
   /** 광고 타입: 'auto'는 랜덤 선택, 특정 타입 지정 가능 */
@@ -28,17 +27,15 @@ const AdRotator = ({
   type = 'auto',
   adSenseSlot = '9944590930',
   kakaoAdUnitId = 'DAN-TuJyMLJV5hB5UXiO',
-  mediaNetCid = 'YOUR_MEDIANET_CID',
   style = { display: 'block', minHeight: '120px', width: '100%' }
 }: AdRotatorProps) => {
   // auto 타입일 경우 랜덤 선택
   const getAdType = () => {
     if (type !== 'auto') return type
     
-    const adTypes: Array<'adsense' | 'kakao' | 'medianet'> = [
+    const adTypes: Array<'adsense' | 'kakao'> = [
       'adsense',
-      'kakao',
-      'medianet'
+      'kakao'
     ]
     // 페이지 로드 시점의 시간 기반으로 선택 (일관성 유지)
     const index = Math.floor(Date.now() / 10000) % adTypes.length
@@ -64,21 +61,14 @@ const AdRotator = ({
           style={style}
         />
       )}
-      {selectedType === 'medianet' && mediaNetCid !== 'YOUR_MEDIANET_CID' && (
+      {/* {selectedType === 'medianet' && mediaNetCid !== 'YOUR_MEDIANET_CID' && (
         <MediaNet
           cid={mediaNetCid}
           style={style}
         />
-      )}
+      )} */}
       {/* 기본값이거나 설정되지 않은 경우 AdSense 사용 */}
       {selectedType === 'kakao' && (!kakaoAdUnitId || kakaoAdUnitId === 'YOUR_KAKAO_AD_UNIT_ID') && (
-        <AdSense
-          adSlot={adSenseSlot}
-          adFormat="horizontal"
-          style={style}
-        />
-      )}
-      {selectedType === 'medianet' && (!mediaNetCid || mediaNetCid === 'YOUR_MEDIANET_CID') && (
         <AdSense
           adSlot={adSenseSlot}
           adFormat="horizontal"
